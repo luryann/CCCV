9200aec3bcfef30e4f61c7e1339b7fc6a0037bdb
Ryan Lu <164600355+luryann@users.noreply.github.com>
2024-10-20 13:04:26 -0700

    fix extension yipeeeee

--- background.js
+++ background.js
@@ -1,43 +1,50 @@
 let isJsDisabled = {};
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-  }]);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
 });
 
 function updateIcon(tabId, isJsEnabled) {
-  const iconName = isJsEnabled ? 'gray' : 'color';
+  console.log(`Updating icon for tab ${tabId}, JS enabled: ${isJsEnabled}`);
+  const iconSuffix = isJsEnabled ? '' : '-gray';
   chrome.pageAction.setIcon({
     tabId: tabId,
     path: {
-      "16": `icons/icon16-${iconName}.png`,
-      "32": `icons/icon32-${iconName}.png`,
-      "48": `icons/icon48-${iconName}.png`,
-      "128": `icons/icon128-${iconName}.png`
+      "16": `icons/icon16${iconSuffix}.png`,
+      "32": `icons/icon32${iconSuffix}.png`,
+      "48": `icons/icon48${iconSuffix}.png`,
+      "128": `icons/icon128${iconSuffix}.png`
     }
   });
 }
 
 function toggleJavaScript(tabId) {
+  console.log(`Toggling JavaScript for tab ${tabId}`);
   if (isJsDisabled[tabId]) {
-    chrome.debugger.detach({tabId: tabId}, function() {
+    // Re-enable JavaScript
+    chrome.debugger.detach({tabId: tabId}, () => {
       if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
+        console.error('Error detaching debugger:', chrome.runtime.lastError);
       } else {
         isJsDisabled[tabId] = false;
         updateIcon(tabId, true);
+        chrome.tabs.reload(tabId); // Refresh only when re-enabling
       }
     });
   } else {
-    chrome.debugger.attach({tabId: tabId}, "1.3", function() {
+    // Disable JavaScript
+    chrome.debugger.attach({tabId: tabId}, "1.3", () => {
       if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
+        console.error('Error attaching debugger:', chrome.runtime.lastError);
       } else {
-        chrome.debugger.sendCommand({tabId: tabId}, "Emulation.setScriptExecutionDisabled", {value: true}, function() {
+        chrome.debugger.sendCommand({tabId: tabId}, "Emulation.setScriptExecutionDisabled", {value: true}, () => {
           if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
+            console.error('Error disabling JavaScript:', chrome.runtime.lastError);
           } else {
             isJsDisabled[tabId] = true;
             updateIcon(tabId, false);
@@ -48,16 +55,34 @@ function toggleJavaScript(tabId) {
   }
 }
 
-chrome.pageAction.onClicked.addListener(function(tab) {
+chrome.pageAction.onClicked.addListener((tab) => {
   toggleJavaScript(tab.id);
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    updateIcon(tabId, !isJsDisabled[tabId]);
+    // Always show the page action
+    chrome.pageAction.show(tabId);
+    
+    // Check if we have a stored state for this tab
+    if (isJsDisabled.hasOwnProperty(tabId)) {
+      updateIcon(tabId, !isJsDisabled[tabId]);
+    } else {
+      // If no stored state, assume JS is enabled
+      updateIcon(tabId, true);
+    }
   }
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
   delete isJsDisabled[tabId];
+});
+
+// Debugger is detached when extension is unloaded
+chrome.runtime.onSuspend.addListener(() => {
+  Object.keys(isJsDisabled).forEach(tabId => {
+    if (isJsDisabled[tabId]) {
+      chrome.debugger.detach({tabId: parseInt(tabId)});
+    }
+  });
 });
